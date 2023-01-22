07c0e1e41bc2d3bfb16c17031c25103f10d84a6b
Even Torbjørnsen <591118@stud.hvl.no>
2023-01-22 22:19:01 +0100

    får ikke helt til, ser på senere. Gjør det vanskeligere enn nødvendig

--- stein, saks & papir/app.js
+++ stein, saks & papir/app.js
@@ -1,8 +1,25 @@
-function stein(){
-    const valg = document.getElementsById("inn");
-    console.log(valg);
+const datavalg = datavalg();
+const spillervalg = document.getElementById("valg");
+
+function spillrunde(datavalg , spillervalg){
+    //Få bruker innput
+    //maskinen skal 
 }
 
-function sjekk() {
-    alert("kake");
+function datavalg(){
+    var data = random(1,3);
+}
+
+function valgomgjører(input) {
+    if (data == 1){
+        return "stein";
+    } if (data == 2 ){
+        return "papir";
+    } else {
+        return "saks";
+    }
+}
+
+function vinner(datavalg , spillervalg) {
+
 }
