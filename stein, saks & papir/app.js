455299bcf76b88f2ccd98c85b92189bb798562f2
Even Torbjørnsen <591118@stud.hvl.no>
2023-02-09 13:50:20 +0100

    litt fremsteg

--- stein, saks & papir/app.js
+++ stein, saks & papir/app.js
@@ -1,16 +1,21 @@
-const datavalg = datavalg();
-const spillervalg = document.getElementById("valg");
+const game = () => {
+    let SpillerScore = 0;
+    let DataScore = 0;
+    let antallSpill = 0;
 
 function spillrunde(datavalg , spillervalg){
-    //Få bruker innput
-    //maskinen skal 
+    const datavalg = datavalg();
+    const spillervalg = document.getElementById("inn");
+
+    vinner (datavalg, spillervalg);
 }
 
 function datavalg(){
     var data = random(1,3);
+    return valgomgjører(data);
 }
 
-function valgomgjører(input) {
+function valgomgjører(data) {
     if (data == 1){
         return "stein";
     } if (data == 2 ){
@@ -21,5 +26,14 @@ function valgomgjører(input) {
 }
 
 function vinner(datavalg , spillervalg) {
+    if (datavalg.equals(spillervalg)){
+        antallSpill++;
+        return "Uavgjort";
+    }
+    if (datavalg == "stein" && spillervalg == "papir"){
+        antallSpill++;
+        SpillerScore++;
+    }
+}
 
 }
