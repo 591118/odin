341fcc6d6372f3fa7c3bb14ce98b1e812db89038
Even Torbjørnsen <591118@stud.hvl.no>
2023-02-16 15:24:34 +0100

    update button

--- jsIntro/click.js
+++ jsIntro/click.js
@@ -1,2 +1,7 @@
-const btn = document.querySelector('#btn');
+const btn1 = document.querySelector('#btn1');
 btn.onclick = () => alert("Hello World");
+
+const btn2 = document.querySelector('#btn2');
+btn.addEventListener('click', () => {
+  alert("Hello World");
+});
