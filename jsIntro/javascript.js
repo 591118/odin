31374eef20da198c2226c092bcc634ecacca3557
Even Torbjørnsen <591118@stud.hvl.no>
2023-02-16 15:17:44 +0100

    ny update

--- jsIntro/javascript.js
+++ jsIntro/javascript.js
@@ -1,15 +1,7 @@
-console.log("hello");
-console.log(23 + 97);
-console.log(1,2,3,4,5,6);
-console.log( (4 + 6 + 9) / 77);
+const container = document.querySelector('#container');
 
-let a = 10;
-const max = 57;
-let actual = -13;
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
 
-let x = actual / max;
-
-console.log( 1 + x);
-
-console.log(a);
-console.log(a*9);
+container.appendChild(content);
