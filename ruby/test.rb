02d10193d62b6b3d6944220536cc546715b1987c
Even Torbjørnsen <591118@stud.hvl.no>
2023-02-23 17:42:40 +0100

    påfyll

--- ruby/test.rb
+++ ruby/test.rb
@@ -26,18 +26,19 @@ if f
 
 
 ##CASE STATEMENT -> Se på
+#Switch i Java -> lager ikke kommentar på B, C eller E
 
 
-  grade = 'F'
-
+grade = 'F'
 did_i_pass = case grade #=> create a variable `did_i_pass` and assign the result of a call to case with the variable grade passed in
   when 'A' then "Hell yeah!"
   when 'D' then "Don't tell your mother."
   else "'YOU SHALL NOT PASS!' -Gandalf"
 end
 
-grade = 'F'
 
+
+grade = 'A'
 case grade
 when 'A'
   puts "You're a genius"
@@ -49,3 +50,21 @@ else
   puts "'YOU SHALL NOT PASS!' -Gandalf"
   fml = true
 end
+
+
+# Unless -> kan være fordel å bruke fremfor if !true        -> tar hvis false
+age = 19
+puts "Welcome to a life of debt." unless age < 18
+
+unless age < 18
+  puts "Down with that sort of thing."
+else
+  puts "Careful now!"
+end
+
+
+#Ternary operator -> sjekker alder, og kommer med passende respons
+age = 19                    #   if true gjennomføres denne               if false gjennomfører denne
+response = age < 18 ? "You still have your entire life ahead of you." : "You're all grown up."
+puts response #=> "You're all grown up."
+
