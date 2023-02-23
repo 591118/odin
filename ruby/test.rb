c98476eb98f7fe93bdb1685f45a328d71a191364
Even Torbjørnsen <591118@stud.hvl.no>
2023-02-23 17:36:49 +0100

    if statment

--- ruby/test.rb
+++ ruby/test.rb
@@ -8,6 +8,18 @@ puts age
 print age;
 print "\n";
 
-puts "Please enter your name:"
-name = gets.chomp
-puts "Hello, #{name}!"
+#puts "Please enter your name:"
+#name = gets.chomp
+#puts "Hello, #{name}!"
+
+
+t = true
+f = false
+
+if f
+    puts "release the goat"
+  elsif f
+    puts "release the shark"
+  else
+    puts "release Kevin the octopus"
+  end
