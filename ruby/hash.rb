a3c01d6a11a35a9ffbeee5efb0dbf43e9e05629c
Even Torbjørnsen <591118@stud.hvl.no>
2023-02-25 22:06:19 +0100

    hash complete

--- ruby/hash.rb
+++ ruby/hash.rb
@@ -13,4 +13,66 @@ my_hash               #=> {}
 hash = { 9 => "nine", :six => 6 }
 
 
+# eks 1
+shoes = {
+    "summer" => "sandals",
+    "winter" => "boots"
+  }
+  
+  shoes["summer"]   #=> "sandals"
 
+  #null når verdien ikke finnes
+  shoes["hiking"]   #=> nil
+
+
+  shoes.fetch("hiking")   #=> KeyError: key not found: "hiking"
+
+
+  #gir en grunnverdi , som er hiking boots, om hash ikke finnes
+  shoes.fetch("hiking", "hiking boots") #=> "hiking boots"
+
+##add
+  shoes["fall"] = "sneakers"
+  shoes     #=> {"summer"=>"sandals", "winter"=>"boots", "fall"=>"sneakers"}
+
+
+shoes["summer"] = "flip-flops"
+shoes     #=> {"summer"=>"flip-flops", "winter"=>"boots", "fall"=>"sneakers"}
+
+#remove
+shoes.delete("summer")    #=> "flip-flops"
+shoes                     #=> {"winter"=>"boots", "fall"=>"sneakers"}
+
+
+#METODER TIL HASH
+books = {
+    "Infinite Jest" => "David Foster Wallace",
+    "Into the Wild" => "Jon Krakauer"
+  }
+  
+  books.keys      #=> ["Infinite Jest", "Into the Wild"]
+  books.values    #=> ["David Foster Wallace", "Jon Krakauer"]
+
+  #Merge
+hash1 = { "a" => 100, "b" => 200 }
+hash2 = { "b" => 254, "c" => 300 }
+hash1.merge(hash2)      #=> { "a" => 100, "b" => 254, "c" => 300 }
+
+
+#SYMBOLS SOM HASH
+
+# 'Rocket' syntax
+american_cars = {
+    :chevrolet => "Corvette",
+    :ford => "Mustang",
+    :dodge => "Ram"
+  }
+  # 'Symbols' syntax
+  japanese_cars = {
+    honda: "Accord",
+    toyota: "Corolla",
+    nissan: "Altima"
+  }
+
+american_cars[:ford]    #=> "Mustang"
+japanese_cars[:honda]   #=> "Accord"
