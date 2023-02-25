06ce0fa43887909849192def35c3c503bb9db12f
Even Torbjørnsen <591118@stud.hvl.no>
2023-02-25 21:31:57 +0100

    hash første commit

---
+++ ruby/hash.rb
@@ -0,0 +1,16 @@
+my_hash = {     #nøkkel venstre -> verdi høyre
+    "a random word" => "ahoy",
+    "Dorothy's math test score" => 94,
+    "an array" => [1, 2, 3],
+    "an empty hash within a hash" => {}
+  }
+
+  #kan opprette med dette også
+my_hash = Hash.new
+my_hash               #=> {}
+
+#evt denne
+hash = { 9 => "nine", :six => 6 }
+
+
+
